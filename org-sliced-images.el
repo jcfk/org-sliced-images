322c8e63aad9c0191ed5970eb69bf5c559b801b1
Jacob Fong <jacobcfong@gmail.com>
2024-03-15 20:23:42 -0700

    Better indentation

--- org-sliced-images.el
+++ org-sliced-images.el
@@ -83,42 +83,42 @@ INCLUDE-LINKED is passed to `org-sliced-images-display-inline-images'."
       (progn
         (org-sliced-images-remove-inline-images beg end)
         (when (called-interactively-p 'interactive)
-	      (message "Inline image display turned off")))
+          (message "Inline image display turned off")))
     (org-sliced-images-display-inline-images include-linked nil beg end)
     (when (called-interactively-p 'interactive)
       (let ((new (org-sliced-images--inline-image-overlay-families beg end)))
         (message (if new
-		             (format "%d images displayed inline"
-			                 (length new))
-		           "No images to display inline"))))))
+                     (format "%d images displayed inline"
+                             (length new))
+                   "No images to display inline"))))))
 
 (defun org-sliced-images--create-inline-image (file width)
   "Create image located at FILE, or return nil.
 WIDTH is the width of the image.  The image may not be created
 according to the value of `org-display-remote-inline-images'."
   (let* ((remote? (file-remote-p file))
-	 (file-or-data
-	  (pcase org-display-remote-inline-images
-	    ((guard (not remote?)) file)
-	    (`download (with-temp-buffer
-			 (set-buffer-multibyte nil)
-			 (insert-file-contents-literally file)
-			 (buffer-string)))
-	    (`cache (let ((revert-without-query '(".")))
-		      (with-current-buffer (find-file-noselect file)
-			(buffer-string))))
-	    (`skip nil)
-	    (other
-	     (message "Invalid value of `org-display-remote-inline-images': %S"
-		      other)
-	     nil))))
+         (file-or-data
+          (pcase org-display-remote-inline-images
+            ((guard (not remote?)) file)
+            (`download (with-temp-buffer
+                         (set-buffer-multibyte nil)
+                         (insert-file-contents-literally file)
+                         (buffer-string)))
+            (`cache (let ((revert-without-query '(".")))
+                      (with-current-buffer (find-file-noselect file)
+                        (buffer-string))))
+            (`skip nil)
+            (other
+             (message "Invalid value of `org-display-remote-inline-images': %S"
+                      other)
+             nil))))
     (when file-or-data
       (create-image file-or-data
-		    (and (image-type-available-p 'imagemagick)
-			 width
-			 'imagemagick)
-		    remote?
-		    :width width :scale 1 :ascent 'center))))
+                    (and (image-type-available-p 'imagemagick)
+                         width
+                         'imagemagick)
+                    remote?
+                    :width width :scale 1 :ascent 'center))))
 
 (defun org-sliced-images--make-inline-image-overlay (start end spec)
   "Make overlay from START to END with display value SPEC.
@@ -168,72 +168,72 @@ buffer boundaries with possible narrowing."
       (when (fboundp 'clear-image-cache) (clear-image-cache)))
     (let ((end (or end (point-max))))
       (org-with-point-at (or beg (point-min))
-	    (let* ((case-fold-search t)
-	           (file-extension-re (image-file-name-regexp))
-	           (link-abbrevs (mapcar #'car
-				                     (append org-link-abbrev-alist-local
-					                         org-link-abbrev-alist)))
-	           ;; Check absolute, relative file names and explicit
-	           ;; "file:" links.  Also check link abbreviations since
-	           ;; some might expand to "file" links.
-	           (file-types-re
-		        (format "\\[\\[\\(?:file%s:\\|attachment:\\|[./~]\\)\\|\\]\\[\\(<?\\(?:file\\|attachment\\):\\)"
-			            (if (not link-abbrevs) ""
-			              (concat "\\|" (regexp-opt link-abbrevs))))))
-	      (while (re-search-forward file-types-re end t)
-	        (let* ((link (org-element-lineage
-			              (save-match-data (org-element-context))
-			              '(link) t))
+        (let* ((case-fold-search t)
+               (file-extension-re (image-file-name-regexp))
+               (link-abbrevs (mapcar #'car
+                                     (append org-link-abbrev-alist-local
+                                             org-link-abbrev-alist)))
+               ;; Check absolute, relative file names and explicit
+               ;; "file:" links.  Also check link abbreviations since
+               ;; some might expand to "file" links.
+               (file-types-re
+                (format "\\[\\[\\(?:file%s:\\|attachment:\\|[./~]\\)\\|\\]\\[\\(<?\\(?:file\\|attachment\\):\\)"
+                        (if (not link-abbrevs) ""
+                          (concat "\\|" (regexp-opt link-abbrevs))))))
+          (while (re-search-forward file-types-re end t)
+            (let* ((link (org-element-lineage
+                          (save-match-data (org-element-context))
+                          '(link) t))
                    (linktype (org-element-property :type link))
-		           (inner-start (match-beginning 1))
-		           (path
-		            (cond
-		             ;; No link at point; no inline image.
-		             ((not link) nil)
-		             ;; File link without a description.  Also handle
-		             ;; INCLUDE-LINKED here since it should have
-		             ;; precedence over the next case.  I.e., if link
-		             ;; contains filenames in both the path and the
-		             ;; description, prioritize the path only when
-		             ;; INCLUDE-LINKED is non-nil.
-		             ((or (not (org-element-property :contents-begin link))
-			              include-linked)
-		              (and (or (equal "file" linktype)
+                   (inner-start (match-beginning 1))
+                   (path
+                    (cond
+                     ;; No link at point; no inline image.
+                     ((not link) nil)
+                     ;; File link without a description.  Also handle
+                     ;; INCLUDE-LINKED here since it should have
+                     ;; precedence over the next case.  I.e., if link
+                     ;; contains filenames in both the path and the
+                     ;; description, prioritize the path only when
+                     ;; INCLUDE-LINKED is non-nil.
+                     ((or (not (org-element-property :contents-begin link))
+                          include-linked)
+                      (and (or (equal "file" linktype)
                                (equal "attachment" linktype))
-			               (org-element-property :path link)))
-		             ;; Link with a description.  Check if description
-		             ;; is a filename.  Even if Org doesn't have syntax
-		             ;; for those -- clickable image -- constructs, fake
-		             ;; them, as in `org-export-insert-image-links'.
-		             ((not inner-start) nil)
-		             (t
-		              (org-with-point-at inner-start
-			            (and (looking-at
-			                  (if (char-equal ?< (char-after inner-start))
-				                  org-link-angle-re
-				                org-link-plain-re))
-			                 ;; File name must fill the whole
-			                 ;; description.
-			                 (= (org-element-property :contents-end link)
-				                (match-end 0))
-			                 (progn
+                           (org-element-property :path link)))
+                     ;; Link with a description.  Check if description
+                     ;; is a filename.  Even if Org doesn't have syntax
+                     ;; for those -- clickable image -- constructs, fake
+                     ;; them, as in `org-export-insert-image-links'.
+                     ((not inner-start) nil)
+                     (t
+                      (org-with-point-at inner-start
+                        (and (looking-at
+                              (if (char-equal ?< (char-after inner-start))
+                                  org-link-angle-re
+                                org-link-plain-re))
+                             ;; File name must fill the whole
+                             ;; description.
+                             (= (org-element-property :contents-end link)
+                                (match-end 0))
+                             (progn
                                (setq linktype (match-string 1))
                                (match-string 2))))))))
-	          (when (and path (string-match-p file-extension-re path))
-		        (let ((file (if (equal "attachment" linktype)
-				                (progn
+              (when (and path (string-match-p file-extension-re path))
+                (let ((file (if (equal "attachment" linktype)
+                                (progn
                                   (require 'org-attach)
-				                  (ignore-errors (org-attach-expand path)))
+                                  (ignore-errors (org-attach-expand path)))
                               (expand-file-name path))))
-		          (when (and file (file-exists-p file))
-		            (let ((width (org-display-inline-image--width link))
-			              (old (get-char-property-and-overlay
-				                (org-element-property :begin link)
-				                'org-image-overlay)))
-		              (if (and (car-safe old) refresh)
+                  (when (and file (file-exists-p file))
+                    (let ((width (org-display-inline-image--width link))
+                          (old (get-char-property-and-overlay
+                                (org-element-property :begin link)
+                                'org-image-overlay)))
+                      (if (and (car-safe old) refresh)
                           (image-flush (overlay-get (cdr old) 'display))
-			            (let ((image (org-sliced-images--create-inline-image file width)))
-			              (when image
+                        (let ((image (org-sliced-images--create-inline-image file width)))
+                          (when image
                             (let* ((image-pixel-cons (image-size image t))
                                    (image-pixel-h (cdr image-pixel-cons))
                                    (image-line-h (/ image-pixel-h (default-font-height) 1.0))
@@ -282,7 +282,7 @@ buffer boundaries with possible narrowing."
   (when (and ov after)
     (when (overlay-get ov 'org-image-overlay)
       (image-flush (cadr (overlay-get ov 'display))))
-	(catch 'break
+    (catch 'break
       (dolist (ovfam org-sliced-images-inline-image-overlay-families)
         (when (memq ov ovfam)
           (setq org-sliced-images-inline-image-overlay-families
