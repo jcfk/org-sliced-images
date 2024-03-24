a9f827d0a9f1121f927c599de8b327fd29255b3e
Jacob Fong <jacobcfong@gmail.com>
2024-03-24 15:01:31 -0700

    Improved header formatting

--- org-sliced-images.el
+++ org-sliced-images.el
@@ -1,22 +1,22 @@
 ;;; org-sliced-images.el --- Sliced inline images in org-mode -*- lexical-binding: t; -*-
-;;
+
 ;; Copyright (C) 2024 Jacob Fong
 ;; Author: Jacob Fong <jacobcfong@gmail.com>
 ;; Version: 0.1
 ;; Homepage: https://github.com/jcfk/org-sliced-images
-;;
+
 ;; Package-Requires: ((emacs "29.1") (org "9.6.15"))
-;;
+
 ;; Permission is hereby granted, free of charge, to any person obtaining a copy
 ;; of this software and associated documentation files (the “Software”), to deal
 ;; in the Software without restriction, including without limitation the rights
 ;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 ;; copies of the Software, and to permit persons to whom the Software is
 ;; furnished to do so, subject to the following conditions:
-;; 
+
 ;; The above copyright notice and this permission notice shall be included in
 ;; all copies or substantial portions of the Software.
-;; 
+
 ;; THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 ;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 ;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,14 +24,14 @@
 ;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 ;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ;; SOFTWARE.
-;;
+
 ;;; Commentary:
-;;
+
 ;; Display images as sliced images in org-mode, like insert-sliced-image. This
 ;; makes scrolling nicer.
-;;
+
 ;; See https://github.com/jcfk/org-sliced-images for more information.
-;;
+
 ;;; Code:
 
 (require 'org)
